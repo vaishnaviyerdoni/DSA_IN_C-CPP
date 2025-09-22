a9fc9bbadee2e7f2db0e362c93f833de338eb5f3
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-09-22 23:48:27 +0530

    right rotate problem

---
+++ CLASS/SESSION-02/02-array.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+//declare necessary func
+void display(int arr[], int N);
+
+int main(void){
+
+    int a[5] = {20, 30, 40, 50, 10};
+
+    printf("The array before right rotate:\n");
+    display(a, 5);
+
+    int i;
+    int temp = a[4];
+    i = 4;
+
+    while(i >= 0) {
+        a[i + 1] = a[i];
+        i = i - 1;
+    } 
+    a[i + 1] = temp;
+
+    printf("The array after right rotate:\n");
+    display(a, 5);
+
+    return 0;
+}
+
+void display(int arr[], int N) {
+    
+    int i;
+    i = 0;
+
+    while(i < N) {
+        printf("arr[%d] : %d\n", i, arr[i]);
+        i = i + 1;
+    }
+}
