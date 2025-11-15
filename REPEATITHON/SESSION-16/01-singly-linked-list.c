33177a044bc5186878065fce89e6b4e2070b88cd
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-11-16 00:05:33 +0530

    repetithon for singly linked list

---
+++ REPEATITHON/SESSION-16/01-singly-linked-list.c
@@ -0,0 +1,217 @@
+//Import all the necessary header files
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+//Define the Symbolic constants
+#define SUCCESS             1
+#define TRUE                1
+#define FALSE               0
+#define LIST_DATA_NOT_FOUND 2
+#define LIST_EMPTY          3
+
+//Node definition layout
+struct node
+{
+    int data;
+    struct node* next;
+}
+
+//Interface Function Declaration
+//List Creation function
+struct node* create_list(void);
+
+//Data addition functions
+int insert_start(struct node* p_list, int new_data);
+int insert_end(struct node* p_list, int new_data);
+int insert_after(struct node* p_list, int existing data, int new_data);
+int insert_before(struct node* p_list, int existing data, int new_data);
+
+//Get functions
+int get_start(struct node* p_list, int* p_start_data);
+int get_end(struct node* p_list, int* p_end_data);
+
+//Pop functions
+int pop_start(struct node* p_list, int* p_start_data);
+int pop_end(struct node* p_list, int* p_end_data);
+
+//Miscellaneous Functions
+int find(struct node* p_list, int f_data);
+int get_list_length(struct node* p_list);
+int is_list_empty(struct node* p_list);
+void show(struct node* p_list, const char* msg);
+
+//List destruction functions
+int destroy_list(struct node* p_list);
+
+//Client of the list
+int main(void)
+{
+    return(0);
+}
+
+//Server side of the List
+//Interface function Defintions
+//List Creation Function
+struct node* create_list(void)
+{
+    struct node* head_node = NULL;
+
+    head_node = (struct node*)malloc(sizeof(struct node));
+    if(NULL == head_node)
+    {
+        puts("Out of Memory");
+        exit(EXIT_FAILURE);
+    }
+
+    head_node -> data = 0;
+    head_node -> next = NULL;
+
+    return(head_node);
+}
+
+//Data addition functions
+int insert_start(struct node* p_list, int new_data)
+{
+    struct node* new_node = NULL;
+
+    new_node = (struct node*)malloc(sizeof(struct node));
+
+    if(NULL == new_node)
+    {
+        puts("Out of Memory");
+        exit(EXIT_FAILURE);
+    }
+
+    new_node -> data = new_data;
+    new_node -> next = p_list -> next;
+    p_list -> next = new_node;
+
+    return(SUCCESS);
+}
+
+int insert_end(struct node* p_list, int new_data)
+{
+    struct node* run = NULL;
+    struct node* new_node = NULL;
+
+    //Step 1 = Allocate and Initialize new node
+    new_node = (struct node*)malloc(sizeof(struct node));
+    if(NULL == new_node)
+    {
+        puts("Out of Memory");
+        exit(EXIT_FAILURE);
+    }
+
+    new_node -> data = new_data;
+    new_node -> next = NULL;
+
+    //Step 2 = Locate the last node by looping through the list
+    run = p_list;
+    while (run->next != NULL)
+    {
+        run = run -> next;
+    }
+
+    //Step 3 - Append the new node at the last position
+    run -> next = new_node;
+
+    return(SUCCESS);
+}
+
+int insert_after(struct node* p_list, int existing_data, int new_data)
+{
+    struct node* existing_node = NULL;
+    struct node* new_node = NULL;
+    struct node* run = NULL;
+
+    //Step 1 - Search for the node containing the first occurence of the existing data
+    run = p_list -> next;
+    while(run != NULL)
+    {
+        if(run->data == existing_data)
+        {
+            break;
+        }
+        run = run -> next;
+    }
+
+    //If existing node is not found then return error saying as such
+    if(NULL == run)
+    {
+        return(LIST_DATA_NOT_FOUND);
+    }
+
+    //Step 2 = Allocate and initialize new node
+    new_node = (struct node*)malloc(sizeof(struct node));
+
+    if(NULL == new_node)
+    {
+        puts("Out of Memory");
+        exit(EXIT_FAILURE);
+    }
+
+    new_node -> data = new_data;
+
+    //Step 3 = Insert the new node at its appropriate position
+    new_node -> next = existing_node -> next;
+    existing_node -> next = new_node;
+
+    return(SUCCESS);
+}
+
+int insert_before(struct node* p_list, int existing data, int new_data)
+{
+    
+}
+
+//Get functions
+int get_start(struct node* p_list, int* p_start_data)
+{
+
+}
+
+int get_end(struct node* p_list, int* p_end_data)
+{
+
+}
+
+//Pop functions
+int pop_start(struct node* p_list, int* p_start_data)
+{
+
+}
+
+int pop_end(struct node* p_list, int* p_end_data)
+{
+
+}
+
+//Miscellaneous Functions
+int find(struct node* p_list, int f_data)
+{
+
+}
+
+int get_list_length(struct node* p_list)
+{
+
+}
+int is_list_empty(struct node* p_list)
+{
+
+}
+
+void show(struct node* p_list, const char* msg)
+{
+
+}
+
+//List destruction functions
+int destroy_list(struct node* p_list)
+{
+
+}
+
+
