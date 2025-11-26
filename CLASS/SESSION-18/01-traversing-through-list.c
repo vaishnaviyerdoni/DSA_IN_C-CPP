34dbb220c0c939e27389bbf5ec84eb63c3afb735
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-11-26 13:42:07 +0530

    function to get to the last node of the linked list

--- CLASS/SESSION-18/01-traversing-through-list.c
+++ CLASS/SESSION-18/01-traversing-through-list.c
@@ -1,1 +1,73 @@
- 
+#include <stdio.h>
+
+struct node
+{
+    int data;
+    struct node* next;
+};
+
+//Function to get to the last node
+void get_last_node(struct node* p_list)
+{
+    struct node* run = NULL;
+
+    run = p_list;
+    while(run -> next != NULL)
+    {
+        run = run -> next;
+    }
+
+    //run is the last node
+}
+
+void get_last_node(struct node* p_list)
+{
+    struct node* run = NULL;
+
+    run = p_list;
+    while(run -> next != NULL)
+    {
+        run = run -> next;
+    }
+
+    //run is the last node
+}
+
+void get_last_node(struct node* p_list)
+{
+    struct node* run = NULL;
+    
+    run = p_list;
+    while(run -> next != NULL)
+    {
+        run = run -> next;
+    }
+
+    //run is the last node
+}
+
+void get_last_node(struct node* p_list)
+{
+    struct node* run = NULL;
+
+    run = p_list;
+    while(run -> next != NULL)
+    {
+        run = run -> next;
+    }
+
+    //run is the last node
+}
+
+void get_last_node(struct node* p_list)
+{
+    struct node* run = p_list;
+
+    run = p_list;
+    while(run -> next != NULL)
+    {
+        run = run -> next;
+    }
+
+    ///run is the last node
+}
