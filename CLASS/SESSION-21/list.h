426ccb5484ae2942c30c4ede2dfb20d75b7b4cab
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-12-04 22:48:51 +0530

    Header file for doubly linked list

---
+++ CLASS/SESSION-21/list.h
@@ -0,0 +1,81 @@
+#ifndef _LIST_H
+#define _LIST_H
+
+#include <stdio.h>
+
+//Symbolic Contants
+#define SUCCESS             1
+#define TRUE                1
+#define FALSE               0
+#define LIST_DATA_NOT_FOUND 2
+#define LIST_EMPTY          3
+
+typedef int data_t;
+typedef int status_t;
+typedef int len_t;
+typedef struct node node_t;
+typedef node_t list_t;
+
+//Node Layout Definition
+struct node
+{
+    data_t data;
+    struct node* prev;
+    struct node* next;
+};
+
+//Server of List
+//Interface Functions
+//List Creation Function
+list_t* create_list(void);
+
+//Data Addition Functions
+status_t insert_start(list_t* p_list, data_t new_data);
+status_t insert_end(list_t*p_list, data_t new_data);
+status_t insert_after(list_t* p_list, data_t e_data, data_t n_data);
+status_t insert_before(list_t* p_list, data_t e_data, data_t n_data);
+
+//Get Data and Pop Data Functions
+status_t get_start(list_t* p_list, data_t* p_start_data);
+status_t get_end(list_t* p_list, data_t* p_end_data);
+
+status_t pop_start(list_t* p_list, data_t* p_start_data);
+status_t pop_end(list_t* p_list, data_t* p_end_data);
+
+//Remove Data Functions
+status_t remove_start(list_t* p_list);
+status_t remove_end(list_t* p_list);
+status_t remove_data(list_t* p_list, data_t r_data);
+
+//Miscellaneous Functions
+status_t is_list_empty(list_t* p_list);
+len_t get_list_length(list_t* p_list);
+status_t find(list_t* p_list, data_t f_data);
+void show(list_t* p_list, const char* msg);
+status_t clear_list(list_t* p_list);
+
+//List Destruction Functions
+status_t destroy_list(list_t** p_list);
+
+//Concat immuable 
+list_t* get_concated_list(list_t* p_list_1, list_t* p_list_2);
+
+//Concat mutable
+status_t concat_lists(list_t* p_list_1, list_t** pp_list_2);
+
+//Merge sorted Lists
+list_t* merge_lists(list_t* p_list_1, list_t* p_list_2);
+
+//Reversal routines
+list_t* get_reversed_list(list_t* p_list);
+status_t reverse_list(list_t* p_list);
+
+//List Auxillary Functions
+static void generic_insert(node_t* p_beg, node_t* p_mid, node_t* p_end);
+static void generic_delete(node_t* p_delete_node);
+static node_t* get_node(data_t new_data);
+static node_t* search_node(list_t* p_list, data_t s_data);
+
+static void* xcalloc(size_t nr_elements, size_t size_per_elements);
+
+#endif 
